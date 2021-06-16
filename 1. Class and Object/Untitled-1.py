a24b73ba13b8bb5dbe2d921b729cd9bfb9accbcf
satyambhatt5 <bhattsatyam793@gmail.com>
2021-06-16 12:05:22 +0530

    first commit

---
+++ 1. Class and Object/Untitled-1.py
@@ -0,0 +1,17 @@
+
+import pandas as pd 
+import numpy as numpy
+import pandas  as pd   
+from sklearn.model_selection import KFold,cross_val_score,cross_val_predict,cross_validate
+from sklearn.linear_model import LinearRegression,LogisticRegression
+from sklearn.feature_extraction import 
+from sklearn.feature_selection import from_model
+from sklearn.impute import MissingIndicator
+from sklearn.preprocessing import OneHotEncoder,OrdinalEncoder,LabelEncoder,LabelBinarizer
+from sklearn.tree import DecisionTreeClassifier,DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor
+
+
+
+
+
